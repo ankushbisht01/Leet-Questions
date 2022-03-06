c7364f62a1fbf393262a4f4ceeff545f993058f8
Ankush Bisht <66681846+ankushbisht01@users.noreply.github.com>
2022-03-06 18:01:34 +0530

    Time: 43 ms (55.02%), Space: 13.8 MB (77.87%) - LeetHub

--- 771-jewels-and-stones/771-jewels-and-stones.py
+++ 771-jewels-and-stones/771-jewels-and-stones.py
@@ -1,7 +1,9 @@
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
         count = 0
+        freq = [False]*58
+        for i in jewels:
+            freq[ord(i) - 65] = True
         for i in stones:
-            if i in jewels:
-                count+=1
-        return count 
+            if(freq[ord(i)-65]): count+=1
+        return count
