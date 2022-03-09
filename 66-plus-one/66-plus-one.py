a5274825e6440c1816e6effd2d2e51961f85e2c0
Ankush Bisht <66681846+ankushbisht01@users.noreply.github.com>
2022-03-09 20:48:27 +0530

    Time: 39 ms (68.89%), Space: 13.8 MB (76.68%) - LeetHub

--- 66-plus-one/66-plus-one.py
+++ 66-plus-one/66-plus-one.py
@@ -1,13 +1,13 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         result = 0 
-        output=[]
-        for i in digits:
-            result = result*10+i
-        result = result+ 1
-        
-        while (result>0):
-            remainder = result%10
-            output.append(remainder)
-            result = result//10
-        return output[::-1]
+        for i in digits :
+            result = result*10 + i
+        result+=1
+        digits = []
+        while result >0 :
+            remainder = result %10
+            digits.append(remainder)
+            result = result //10
+        return digits[::-1]
+                
