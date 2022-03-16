e6e25167e7e55a72fab1bf14d8645e6d931deaf6
Ankush Bisht <66681846+ankushbisht01@users.noreply.github.com>
2022-03-16 15:21:54 +0530

    Time: 70 ms (51.68%), Space: 14.8 MB (57.34%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.py
+++ 35-search-insert-position/35-search-insert-position.py
@@ -11,19 +11,5 @@ class Solution:
                 end = mid -1
             else:
                 start = mid +1
-        else:
-            start = 0 
-            end = len(nums)-1
-
-            while start<=end:
-                mid = (start+end)//2
-                if nums[end]<target:
-                    return end+1
-                elif nums[start]>target:
-                    return start  
-                elif nums[mid]>target:
-                    end = mid -1
-                else:
-                    start = mid +1
-            return mid
+        return start
             
